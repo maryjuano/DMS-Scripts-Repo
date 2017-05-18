3e4b94312b16ecb63a349c341101acc8e6556442
raptoron <raphael.herrera7@gmail.com>
2017-05-18 15:14:22 +0800

    Fix for Bug # 16147

--- edit-purchaseorder.js
+++ edit-purchaseorder.js
@@ -23,6 +23,7 @@ $(document).ready(function () {
         $("#EntityFormView fieldset").addClass("permanent-disabled");
         $('#gsc_vpostatus').css({ "pointer-events": "none", "cursor": "default" });
         $('#gsc_approvalstatus').css({ "pointer-events": "none", "cursor": "default" });
+        $("#gsc_remarks").css('resize', 'none');
     }
     else {
         createCancelButton();
